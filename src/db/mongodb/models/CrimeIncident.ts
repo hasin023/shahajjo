cf9be087cd2a09a9557b779b21210cd5c3ff151a
Khandakar227 <xenonknight54@gmail.com>
2025-02-10 12:51:39 +0600

    Crime incident collection, auth in ui

--- src/db/mongodb/models/CrimeIncident.ts
+++ src/db/mongodb/models/CrimeIncident.ts
@@ -5,6 +5,16 @@ export interface ICrimeReport {
 }
 
 const CrimeReportSchema = new Schema<ICrimeReport>({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    location: { type: String, required: true },
+    images: [{ type: String }], // Image URLs
+    reportedBy: { type: Schema.Types.ObjectId, ref: "user", required: true },
+    upvotes: { type: Number, default: 0 }, // Number of upvotes
+    downvotes: { type: Number, default: 0 }, // Number of downvotes
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+    verified: { type: Boolean, default: false }, // Admin verification status
+    createdAt: { type: Date, default: Date.now }
 });
 
 const CrimeReport = models?.crimeReport as Model<ICrimeReport> || model('crimeReport', CrimeReportSchema);
